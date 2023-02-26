8f6d40cc364c3c623ccb0ae91539d02c5fae3291
yukihir0 <yukihiro.cotori@gmail.com>
2023-02-26 12:43:07 +0000

    chapter06

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -2,20 +2,40 @@ package server
 
 import (
 	"context"
+	"flag"
 	"net"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	api "github.com/yukihir0/proglog/api/v1"
 	"github.com/yukihir0/proglog/internal/auth"
 	tlsconfig "github.com/yukihir0/proglog/internal/config"
 	"github.com/yukihir0/proglog/internal/log"
+	"go.opencensus.io/examples/exporter"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 )
+
+var debug = flag.Bool("debug", false, "Enable observability for debugging.")
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	if *debug {
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			panic(err)
+		}
+
+		zap.ReplaceGlobals(logger)
+	}
+
+	os.Exit(m.Run())
+}
 
 func TestServer(t *testing.T) {
 	for scenario, fn := range map[string]func(
@@ -73,6 +93,30 @@ func setupTest(t *testing.T, fn func(*Config)) (rootClient api.LogClient, nobody
 		fn(config)
 	}
 
+	// メトリクスとトレースの設定をする
+	var telemetryExporter *exporter.LogExporter
+	if *debug {
+		// メトリクスファイル
+		metricsLogFile, err := os.CreateTemp("", "metrics-*.log")
+		require.NoError(t, err)
+		t.Logf("metrics log file: %s", metricsLogFile.Name())
+
+		// トレースファイル
+		tracesLogFile, err := os.CreateTemp("", "traces-*.log")
+		require.NoError(t, err)
+		t.Logf("traces log file: %s", tracesLogFile.Name())
+
+		telemetryExporter, err = exporter.NewLogExporter(exporter.Options{
+			MetricsLogFile:    metricsLogFile.Name(),
+			TracesLogFile:     tracesLogFile.Name(),
+			ReportingInterval: time.Second,
+		})
+		require.NoError(t, err)
+
+		err = telemetryExporter.Start()
+		require.NoError(t, err)
+	}
+
 	// サーバを設定する
 	server, err := NewGRPCServer(
 		config,
@@ -123,6 +167,11 @@ func setupTest(t *testing.T, fn func(*Config)) (rootClient api.LogClient, nobody
 		server.Stop()
 		l.Close()
 		clog.Remove()
+		if telemetryExporter != nil {
+			time.Sleep(1500 * time.Millisecond)
+			telemetryExporter.Stop()
+			telemetryExporter.Close()
+		}
 	}
 }
 
